f665ab4e0fef79a1a5ac04e9aebcd91cfb542528
frogEUN <eunhasu801@gmail.com>
2023-08-05 10:34:56 +0900

    0805

---
+++ first.py
@@ -0,0 +1,14 @@
+# 2장 - 데이터: 타입, 값, 변수, 이름
+a = b = c = 2
+print(a)
+print(b)
+print(c)
+print(type(a))
+prince = 99
+print(prince)
+
+# 3장 - 숫자
+print(10 % 3)
+print(divmod(10, 3))
+print(chr(65))
+print(ord('A'))
