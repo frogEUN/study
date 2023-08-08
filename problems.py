7ecb2458bdeb39e6ece9cc9d43760cb0662980e9
frogEUN <eunhasu801@gmail.com>
2023-08-08 15:03:07 +0900

    10809, 2675

--- problems.py
+++ problems.py
@@ -1,21 +1,17 @@
-# 27866번
-s = input()
-i = int(input())
-print(s[i-1])
-
-# 2743번
+# 10809번
 word = input()
-print(len(word))
-# len()  : 문자열을 인자로 받아, 문자열의 길이를 숫자로 반환
+for i in range(ord('a'), ord('z')+1):
+    print(word.find(chr(i)), end=' ')
 
-# 9086번
+# 2675번
 t = int(input())
-answers = list()
-for i in range(1, t+1):
-    word = input()
-    answers.append(word[0]+word[-1])
+words = list()
+
 for i in range(0, t):
-    print(answers[i])
-# append : 원소 마지막에 추가
-# insert : 리스트.index(입력할 index, 값)
-# extend : 리스트.extend(추가할 리스트)
+    rANDs = input().split()
+    words.append(rANDs)
+
+for i in range(0, t):
+    for k in range(0, len(words[i][1])):
+        print(words[i][1][k] * int(words[i][0]), end='')
+    print()
