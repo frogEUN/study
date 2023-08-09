488d6bb67c22e878fbe9ba4031ef3aa760032433
frogEUN <eunhasu801@gmail.com>
2023-08-09 18:07:02 +0900

    25083, 3003

--- problems.py
+++ problems.py
@@ -1,24 +1,14 @@
-# 2753번
-# 윤년이면 1, 아니면 0 출력
-# 연도가 4의 배수이면서 100의 배수가 아닐때 또는 400의 배수일때
-year = int(input())
-if year % 4 == 0:
-    if year % 100 != 0:
-        print(1)
-    elif year % 400 == 0:
-        print(1)
-    else:
-        print(0)
-else:
-    print(0)
-#  // 몫을 리턴, % 나머지값을 리턴
+# 25083번
+print('         ,r\'\"7')
+print('r`-_   ,\'  ,/')
+print(' \\.\". L_r\'')
+print('   `~\\/')
+print('      |')
+print('      |')
 
-
-# 10950번
-# 첫째 줄에 테스트 케이스의 개수 T
-# A, B -> A+B 출력
-t = int(input())
-for i in range(0, t):
-    nums = input().split()
-    print(int(nums[0]) + int(nums[1]))
-
+# 3003번
+# 킹 1, 퀸 1, 룩 2, 비숍 2, 나이트 2, 폰 8
+dong = input().split()
+answers = [1, 1, 2, 2, 2, 8]
+for i in range(0, len(dong)):
+    print(answers[i] - int(dong[i]), end=' ')
