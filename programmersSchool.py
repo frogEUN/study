4ac2bf3624c9ad64b03ef92b167a2c5b998db80b
frogEUN <eunhasu801@gmail.com>
2023-08-09 20:33:40 +0900

    코딩 기초 트레이닝 Day1

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,25 +1,20 @@
-# Lv0. 문자열이 몇 번 등장하는지 세기
-# 문자열 myString과 pat이 주어짐.
-# myString에서 pat이 등장하는 횟수를 return하는 solution함수 완성하기
-''' 풀이1
-def solution(myString, pat):
-    answer = 0
-    temp = 0
-    while True:
-        idx = myString.find(pat, temp)
-        if idx == -1:
-            break
-        answer += 1
-        temp = idx + 1
-    return answer
-'''
+# 코딩 기초 트레이닝
+# a와 b 출력하기
+a, b = map(int, input().strip().split(' '))
+print(f'a = {a}')
+print(f'b = {b}')
 
 
-''' 풀이2
-def solution(myString, pat):
-    answer = 0
-    for i in range(len(myString)):
-        if myString[i:i+len(pat)] == pat:
-            answer += 1
-    return answer
-'''
+# 문자열 반복해서 출력하기
+a, b = input().strip().split(' ')
+b = int(b)
+print(a * b)
+
+
+# 대소문자 바꿔서 출력하기
+str = input()
+print(str.swapcase())
+
+
+# 특수문자 출력하기
+print('!@#$%^&*(\\\'\"<>?:;')
