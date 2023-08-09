8a2299e125463ddbcb503f82c836312d6d308431
frogEUN <eunhasu801@gmail.com>
2023-08-09 17:56:43 +0900

    2753, 10950

--- problems.py
+++ problems.py
@@ -1,12 +1,24 @@
-# 11654번
-print(ord(input()))
+# 2753번
+# 윤년이면 1, 아니면 0 출력
+# 연도가 4의 배수이면서 100의 배수가 아닐때 또는 400의 배수일때
+year = int(input())
+if year % 4 == 0:
+    if year % 100 != 0:
+        print(1)
+    elif year % 400 == 0:
+        print(1)
+    else:
+        print(0)
+else:
+    print(0)
+#  // 몫을 리턴, % 나머지값을 리턴
 
-# 11720번
-# 첫째 줄에 숫자의 개수, 둘째 줄에 숫자 n개 공백없이.
-# 숫자들의 합을 출력
-n = int(input())
-result = 0
-numbers = input()
-for i in range(0, n):
-    result = result + int(numbers[i])
-print(result)
+
+# 10950번
+# 첫째 줄에 테스트 케이스의 개수 T
+# A, B -> A+B 출력
+t = int(input())
+for i in range(0, t):
+    nums = input().split()
+    print(int(nums[0]) + int(nums[1]))
+
