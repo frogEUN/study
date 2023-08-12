77103a5e2d0ea2d9a225a131bd4297cde5d18574
frogEUN <eunhasu801@gmail.com>
2023-08-12 10:02:51 +0900

    코딩 기초 트레이닝 Day7

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,66 +1,68 @@
 # 코딩 기초 트레이닝
 
-# 마지막 두 원소
-def solution(num_list):
-    answer = []
-    if num_list[-1] > num_list[-2]:
-        answer.append(num_list[-1] - num_list[-2])
-    else:
-        answer.append(num_list[-1]*2)
-    return num_list + answer
-
-
-# 수 조작하기 1
-def solution(n, control):
-    for i in control:
-        if i == 'w':
-            n += 1
-        elif i == 's':
-            n -= 1
-        elif i == 'd':
-            n += 10
-        elif i == 'a':
-            n -= 10
-    return n
-
-
-# 수 조작하기 2
-def solution(numLog):
-    answer = ''
-    for i in range(1, len(numLog)):
-        temp = numLog[i] - numLog[i-1]
-        if temp == 1:
-            answer = answer + 'w'
-        elif temp == -1:
-            answer = answer + 's'
-        elif temp == 10:
-            answer = answer + 'd'
-        elif temp == -10:
-            answer = answer + 'a'
-    return answer
-
-
-# 수열과 구간 쿼리 3
+# 수열과 구간 쿼리 4
 def solution(arr, queries):
-    for q in queries:
-        arr[q[0]], arr[q[1]] = arr[q[1]], arr[q[0]]
-    return arr
-
-
-# 수열과 구간 쿼리 2
-def solution(arr, queries):
-    answer = []
-    small = -1
     for q in queries:
         for i in range(len(arr)):
             if q[0] <= i <= q[1]:
-                if arr[i] > q[2]:
-                    if small == -1:
-                        small = arr[i]
-                    else:
-                        if small > arr[i]:
-                            small = arr[i]
-        answer.append(small)
-        small = -1
+                if i % q[2] == 0:
+                    arr[i] = arr[i] + 1
+    return arr
+
+
+# 배열 만들기2
+def solution(l, r):
+    answer = []
+    temp = True
+    for i in range(l, r+1):
+        i = str(i)
+        for k in range(len(i)):
+            if i[k] == '5' or i[k] == '0':
+                continue
+            else:
+                temp = False
+                break
+        if temp:
+            answer.append(int(i))
+        temp = True
+    if len(answer) == 0:
+        return [-1]
     return answer
 
+
+# 카운트 업
+def solution(start, end):
+    answer = []
+    for i in range(start, end + 1):
+        answer.append(i)
+    return answer
+
+
+# 콜라츠 수열
+def solution(n):
+    answer = [n]
+    while True:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        answer.append(n)
+        if n == 1:
+            break
+    return answer
+
+
+# 배열 만들기 4
+def solution(arr):
+    stk = []
+    i = 0
+    while i < len(arr):
+        if len(stk) == 0:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        elif stk[-1] >= arr[i]:
+            del stk[-1]
+    return stk
