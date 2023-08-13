0ebae4411b04d4d441ddd78ecf4260cd5d8ccb61
frogEUN <eunhasu801@gmail.com>
2023-08-13 22:26:27 +0900

    0813 - 7장 중 딕셔너리

--- first.py
+++ first.py
@@ -1,13 +1,17 @@
-# 7장 - 튜플과 리스트
+# 8장 - 딕셔너리와 셋
 
-# 7.1
-year_lists = [2001 + i for i in range(6)]
-print(year_lists)
-
-# 7.4 ~ 7.7
-things = ["mozzarella", "cinderella", "salmonella"]
-things[1].capitalize()
-print(things)
-print(things[0].upper())
-del things[2]
-print(things)
+print("[딕셔너리!]")
+heroes = dict(kiriko="support", genji="dps", winston="tank")
+# [key]와 get()의 차이
+# 딕셔너리에 해당 키가 없으면
+# [key]는 예외 발생
+# get()은 none, 또는 두번째 파라미터로 넣은 값을 반환
+print("heroes  :  ", heroes)
+print(".keys()  :  ", heroes.keys())
+print(".values()  :  ", heroes.values())
+print(".items()  :  ", heroes.items())
+# 결합하기 :  {**dict1, **dict2}    or     dict1.update(dict2)
+# del dict1[key1] -> 삭제
+# dict1.pop(key1) -> 해당 값 반환하고 삭제
+# 모든 항목 삭제하기 : .clear()
+# 딕셔너리 컴프리헨션 {키 표현식 : 값 표현식 for 표현식 in 순회 가능한 객체}
