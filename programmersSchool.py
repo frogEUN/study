420c989945b38f072c2e6bb587db6f163f598a4f
frogEUN <eunhasu801@gmail.com>
2023-08-15 18:06:47 +0900

    코딩 기초 트레이닝 Day14

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,37 +1,47 @@
 # 코딩 기초 트레이닝
 
-# n 번째 원소부터
-def solution(num_list, n):
-    return num_list[n-1:]
-
-
-# 순서 바꾸기
-def solution(num_list, n):
-    return num_list[n:] + num_list[:n]
-
-
-# 왼쪽 오른쪽
-def solution(str_list):
-    if str_list.count('l') == str_list.count('r') == 0:
-        return []
-    elif str_list.count('l') == 0:
-        return str_list[str_list.index('r') + 1:]
-    elif str_list.count('r') == 0:
-        return str_list[:str_list.index('l')]
-    l = str_list.index('l')
-    r = str_list.index('r')
-    if l < r:
-        return str_list[:l]
+# 홀수 vs 짝수
+def solution(num_list):
+    even = 0
+    odd = 0
+    for i in range(len(num_list)):
+        if (i+1) % 2 == 0:
+            even += num_list[i]
+        else:
+            odd += num_list[i]
+    if even >= odd:
+        return even
     else:
-        return str_list[r + 1:]
+        return odd
 
 
-# n 번째 원소까지
-def solution(num_list, n):
-    return num_list[:n]
+# 5명씩
+def solution(names):
+    return names[::5]
 
 
-# n개 간격의 원소들
-def solution(num_list, n):
-    return num_list[::n]
+# 할 일 목록
+def solution(todo_list, finished):
+    answer = []
+    for i in range(len(todo_list)):
+        if finished[i] == False:  # not finished[i]
+            answer.append(todo_list[i])
+    return answer
 
+
+# n보다 커질 때까지 더하기
+def solution(numbers, n):
+    result = 0
+    for num in numbers:
+        result += num
+        if result > n:
+            return result
+
+
+# 수열과 구간 쿼리 1
+def solution(arr, queries):
+    for q in queries:
+        for i in range(len(arr)):
+            if q[0] <= i <= q[1]:
+                arr[i] = arr[i] + 1
+    return arr
