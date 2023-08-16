8b5e97440dd66aa084e82cb410aeabbf1593f042
frogEUN <eunhasu801@gmail.com>
2023-08-16 09:33:42 +0900

    코딩 기초 트레이닝 Day15

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,47 +1,63 @@
 # 코딩 기초 트레이닝
 
-# 홀수 vs 짝수
-def solution(num_list):
-    even = 0
-    odd = 0
-    for i in range(len(num_list)):
-        if (i+1) % 2 == 0:
-            even += num_list[i]
-        else:
-            odd += num_list[i]
-    if even >= odd:
-        return even
-    else:
-        return odd
-
-
-# 5명씩
-def solution(names):
-    return names[::5]
-
-
-# 할 일 목록
-def solution(todo_list, finished):
-    answer = []
-    for i in range(len(todo_list)):
-        if finished[i] == False:  # not finished[i]
-            answer.append(todo_list[i])
-    return answer
-
-
-# n보다 커질 때까지 더하기
-def solution(numbers, n):
-    result = 0
-    for num in numbers:
-        result += num
-        if result > n:
-            return result
-
-
-# 수열과 구간 쿼리 1
-def solution(arr, queries):
-    for q in queries:
-        for i in range(len(arr)):
-            if q[0] <= i <= q[1]:
-                arr[i] = arr[i] + 1
+# 조건에 맞게 수열 변환하기 1
+def solution(arr):
+    for i in range(len(arr)):
+        if arr[i] >= 50 and arr[i] % 2 == 0:
+            arr[i] = arr[i] // 2
+        elif arr[i] < 50 and arr[i] % 2 == 1:
+            arr[i] = arr[i] * 2
     return arr
+
+
+# 조건에 맞게 수열 변환하기 2
+def solution(arr):
+    x = 0
+    arr_x = arr
+    while True:
+        arr_x = arr[:]
+        for i in range(len(arr)):
+            if arr[i] >= 50 and arr[i] % 2 == 0:
+                arr[i] = arr[i] // 2
+            elif arr[i] < 50 and arr[i] % 2 == 1:
+                arr[i] = arr[i] * 2 + 1
+        x = x + 1
+        if arr_x == arr:
+            return x-1
+
+
+# 1로 만들기
+def solution(num_list):
+    result = 0
+    for n in num_list:
+        while True:
+            if n == 1:
+                break
+            elif n % 2 == 0:
+                n = n // 2
+                result += 1
+            else:
+                n = (n - 1) // 2
+                result += 1
+    return result
+
+
+# 길이에 따른 연산
+def solution(num_list):
+    answer = 0
+    if len(num_list) >= 11:
+        for n in num_list:
+            answer += n
+        return answer
+    else:
+        answer = 1
+        for n in num_list:
+            answer *= n
+        return answer
+
+
+# 원하는 문자열 찾기
+def solution(myString, pat):
+    if myString.upper().find(pat.upper()) == -1:
+        return 0
+    return 1
