79022ecbf91e20c5867d2aaf9a6d053bd4b74349
frogEUN <eunhasu801@gmail.com>
2023-08-16 09:40:25 +0900

    코딩 기초 트레이닝 Day16

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,63 +1,40 @@
 # 코딩 기초 트레이닝
 
-# 조건에 맞게 수열 변환하기 1
-def solution(arr):
-    for i in range(len(arr)):
-        if arr[i] >= 50 and arr[i] % 2 == 0:
-            arr[i] = arr[i] // 2
-        elif arr[i] < 50 and arr[i] % 2 == 1:
-            arr[i] = arr[i] * 2
-    return arr
+# 대문자로 바꾸기
+def solution(myString):
+   return myString.upper()
 
 
-# 조건에 맞게 수열 변환하기 2
-def solution(arr):
-    x = 0
-    arr_x = arr
-    while True:
-        arr_x = arr[:]
-        for i in range(len(arr)):
-            if arr[i] >= 50 and arr[i] % 2 == 0:
-                arr[i] = arr[i] // 2
-            elif arr[i] < 50 and arr[i] % 2 == 1:
-                arr[i] = arr[i] * 2 + 1
-        x = x + 1
-        if arr_x == arr:
-            return x-1
+# 소문자로 바꾸기
+def solution(myString):
+    return myString.lower()
 
 
-# 1로 만들기
-def solution(num_list):
-    result = 0
-    for n in num_list:
-        while True:
-            if n == 1:
-                break
-            elif n % 2 == 0:
-                n = n // 2
-                result += 1
-            else:
-                n = (n - 1) // 2
-                result += 1
-    return result
+# 배열에서 문자열 대소문자 변환하기
+def solution(strArr):
+    for i in range(len(strArr)):
+        if i % 2 == 0:
+            strArr[i] = strArr[i].lower()
+        else:
+            strArr[i] = strArr[i].upper()
+    return strArr
 
 
-# 길이에 따른 연산
-def solution(num_list):
-    answer = 0
-    if len(num_list) >= 11:
-        for n in num_list:
-            answer += n
-        return answer
-    else:
-        answer = 1
-        for n in num_list:
-            answer *= n
-        return answer
+# A 강조하기
+def solution(myString):
+    myString = list(myString)
+    for i in range(len(myString)):
+        if myString[i] == 'a' or myString[i] == 'A':
+            myString[i] = myString[i].upper()
+        else:
+            myString[i] = myString[i].lower()
+    return ''.join(myString)
 
 
-# 원하는 문자열 찾기
-def solution(myString, pat):
-    if myString.upper().find(pat.upper()) == -1:
-        return 0
-    return 1
+# 특정한 문자를 대문자로 바꾸기
+def solution(my_string, alp):
+    m = list(my_string)
+    for i in range(len(m)):
+        if m[i] == alp:
+            m[i] = m[i].upper()
+    return ''.join(m)
