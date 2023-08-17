e9c9e010b066392c59fa845d4a37cd842e7f76e3
frogEUN <eunhasu801@gmail.com>
2023-08-17 21:52:42 +0900

    코딩 기초 트레이닝 Day18

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,44 +1,56 @@
 # 코딩 기초 트레이닝
 
-# 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기
-def solution(myString, pat):
-    if myString.count(pat) == 1:
-        return myString[:myString.find(pat)]+pat
-    temp = myString[::-1]
-    tempPat = pat[::-1]
-    return myString[:len(myString)-temp.find(tempPat)-1]+pat
+# x 사이의 개수
+def solution(myString):
+    answer = myString.split('x')
+    result = list()
+    for a in answer:
+        result.append(len(a))
+    return result
 
 
-# 문자열이 몇 번 등장하는지 세기
-def solution(myString, pat):
-    answer = 0
-    temp = 0
-    while True:
-        idx = myString.find(pat, temp)
-        if idx == -1:
-            break
-        answer += 1
-        temp = idx + 1
+# 문자열 잘라서 정렬하기
+def solution(myString):
+    myString = myString.strip('x')
+    answer = myString.split('x')
+    answer = [a.strip('x') for a in answer if a.strip('x') != '']
+    answer.sort()
     return answer
 
 
-# ad 제거하기
-def solution(strArr):
-    delList = list()
-    for i in range(len(strArr)):
-        if strArr[i].find("ad") >= 0:
-            delList.append(i)
-    delList.sort(reverse=True)
-    for k in delList:
-        del strArr[k]
-    return strArr
+# 간단한 식 계산하기
+def solution(binomial):
+    temp = binomial.split()
+    a = int(temp[0])
+    op = temp[1]
+    b = int(temp[2])
+    if op == "+":
+        return a + b
+    elif op == "-":
+        return a - b
+    elif op == "*":
+        return a * b
 
 
-# 공백으로 구분하기 1
-def solution(my_string):
-    return my_string.split()
+# 문자열 바꿔서 찾기
+def solution(myString, pat):
+    myString = list(myString)
+    for i in range(len(myString)):
+        if myString[i] == "A":
+            myString[i] = "B"
+        else:
+            myString[i] = "A"
+    myString = ''.join(myString)
+    temp = myString.find(pat)
+    if temp == -1:
+        return 0
+    return 1
 
 
-# 공백으로 구분하기 2
-def solution(my_string):
-    return my_string.split()
+# rny_string
+def solution(rny_string):
+    my = list(rny_string)
+    for i in range(len(my)):
+        if my[i] == "m":
+            my[i] = "rn"
+    return ''.join(my)
