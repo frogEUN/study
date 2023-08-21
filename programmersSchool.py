a65c98b0dfc2d721faf01909222955b9a053ead2
frogEUN <eunhasu801@gmail.com>
2023-08-21 09:42:52 +0900

    코딩 기초 트레이닝 Day22

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,97 +1,31 @@
 # 코딩 기초 트레이닝
 
-# 배열의 길이를 2의 거듭제곱으로 만들기
-def solution(arr):
-    while True:
-        n = len(arr)
-        if n & (n-1) == 0:
-            return arr
-        arr.append(0)
-    # n이 2의 거듭제곱이라면 n과 n-1을 & 연산 했을 때 0이 나온다
+# 0 떼기
+def solution(n_str):
+    return n_str.lstrip('0')
 
 
-# 배열 비교하기
-def solution(arr1, arr2):
-    a1, a2 = 0, 0
-    if len(arr1) > len(arr2):
-        return 1
-    elif len(arr1) < len(arr2):
-        return -1
-    else:
-        for a in arr1:
-            a1 += a
-        for a in arr2:
-            a2 += a
-        if a1 > a2:
-            return 1
-        elif a1 < a2:
-            return -1
-        else:
-            return 0
+# 두 수의 합
+def solution(a, b):
+    return str(int(a)+int(b))
 
 
-# 문자열 묶기
-def solution(strArr):
-    answer = {1: 0, 2: 0}
-    for str in strArr:
-        if len(str) in answer:
-            answer[len(str)] += 1
-        else:
-            answer[len(str)] = 1
-    max_ = 1
-    max_value = 0
-    for key, value in answer.items():
-        if value > max_value:
-            max_value = value
-            max_ = key
-    return max_value
+# 문자열로 변환
+def solution(n):
+    return str(n)
 
 
-# 배열의 길이에 따라 다른 연산하기
-def solution(arr, n):
-    k = 0
-    if len(arr) % 2 == 0:
-        k = 1
-    else:
-        k = 0
-    for i in range(len(arr)):
-        if i % 2 == k:
-            arr[i] = arr[i] + n
+# 배열의 원소 삭제하기
+def solution(arr, delete_list):
+    for d in delete_list:
+        if d in arr:
+            del arr[arr.index(d)]
     return arr
 
 
-# 뒤에서 5등까지
-def solution(num_list):
-    num_list.sort()
-    for i in range(5):
-        del num_list[0]
-    return num_list
-
-
-# 전국 대회 선발 고사
-def solution(rank, attendance):
-    stu = list()
-    for i in range(len(rank)):
-        if attendance[i]:
-            stu.append((i, rank[i]))
-    stu.sort(key=lambda x: x[1])
-    a, b, c = stu[:3]
-    return 10000 * a[0] + 100 * b[0] + c[0]
-
-
-# 정수 부분
-def solution(flo):
-    return int(flo)
-
-
-# 문자열 정수의 합
-def solution(num_str):
-    answer = 0
-    for n in num_str:
-        answer += int(n)
-    return answer
-
-
-# 문자열을 정수로 변환하기
-def solution(n_str):
-    return int(n_str)
+# 부분 문자열인지 확인하기
+def solution(my_string, target):
+    if target in my_string:
+        return 1
+    else:
+        return 0
