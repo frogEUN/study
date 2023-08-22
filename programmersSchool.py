81df650cd9314aebdf0cc37517077fad830a0d76
frogEUN <eunhasu801@gmail.com>
2023-08-22 09:37:23 +0900

    코딩 기초 트레이닝 Day23

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,31 +1,58 @@
 # 코딩 기초 트레이닝
 
-# 0 떼기
-def solution(n_str):
-    return n_str.lstrip('0')
-
-
-# 두 수의 합
-def solution(a, b):
-    return str(int(a)+int(b))
-
-
-# 문자열로 변환
-def solution(n):
-    return str(n)
-
-
-# 배열의 원소 삭제하기
-def solution(arr, delete_list):
-    for d in delete_list:
-        if d in arr:
-            del arr[arr.index(d)]
-    return arr
-
-
-# 부분 문자열인지 확인하기
-def solution(my_string, target):
-    if target in my_string:
+# 부분 문자열
+def solution(str1, str2):
+    if str1 in str2:
         return 1
     else:
         return 0
+
+
+# 꼬리 문자열
+def solution(str_list, ex):
+    answer = ''
+    for str in str_list:
+        if ex in str:
+            continue
+        else:
+            answer = answer + str
+    return answer
+
+
+# 정수 찾기
+def solution(num_list, n):
+    if num_list.count(n) == 0:
+        return 0
+    return 1
+
+
+# 주사위 게임 1
+def solution(a, b):
+    answer = 0
+    if a % 2 == 1 and b % 2 == 1:
+        answer += a*a + b*b
+    elif a % 2 == 1 or b % 2 == 1:
+        answer += 2 * (a + b)
+    else:
+        answer += abs(a-b)
+    return answer
+
+
+# 날짜 비교하기
+def solution(date1, date2):
+    if date1[0] < date2[0]:
+        return 1
+    elif date1[0] > date2[0]:
+        return 0
+    else:
+        if date1[1] < date2[1]:
+            return 1
+        elif date1[1] > date2[1]:
+            return 0
+        else:
+            if date1[2] < date2[2]:
+                return 1
+            elif date1[2] > date2[2]:
+                return 0
+            else:
+                return 0
