1686a29ab97c1867580a387ac4eae124efa1c20f
frogEUN <eunhasu801@gmail.com>
2023-08-24 09:50:55 +0900

    코딩 기초 트레이닝 Day25 (1)

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,58 +1,40 @@
 # 코딩 기초 트레이닝
 
-# 커피 심부름
-def solution(order):
-    answer = 0
-    for o in order:
-        if 'americano' in o or o == 'anything':
-            answer += 4500
-        else:
-            answer += 5000
-    return answer
+# 특별한 이차원 배열 2
+def solution(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != arr[j][i]:
+                return 0
+    return 1
 
 
-# 그림 확대
-def solution(picture, k):
-    answer = []
-    temp = ''
-    for p in picture:
-        for s in p:
-            temp += s * k
-        for i in range(k):
-            answer.append(temp)
-        temp = ''
-    return answer
-
-
-# 조건에 맞게 수열 변환하기 3
-def solution(arr, k):
-    if k % 2 == 0:
+# 정사각형으로 만들기
+def solution(arr):
+    a = len(arr)
+    b = len(arr[0])
+    if a > b:
         for i in range(len(arr)):
-            arr[i] = arr[i] + k
-    else:
-        for i in range(len(arr)):
-            arr[i] = arr[i] * k
+            for k in range(a - b):
+                arr[i].append(0)
+    elif a < b:
+        for i in range(b - a):
+            temp = []
+            for k in range(b):
+                temp.append(0)
+            arr.append(temp)
+            temp = []
     return arr
 
 
-# l로 만들기
-def solution(myString):
-    my = list(myString)
-    for i in range(len(my)):
-        if ord(my[i]) < ord('l'):
-            my[i] = 'l'
-    return ''.join(my)
-
-
-# 특별한 이차원 배열 1
-def solution(n):
-    answer = []
-    for i in range(n):
-        temp = []
-        for k in range(n):
-            if i == k:
-                temp.append(1)
-            else:
-                temp.append(0)
-        answer.append(temp)
+# 이차원 배열 대각선 순회하기
+def solution(board, k):
+    answer = 0
+    temp = []
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if i + j <= k:
+                temp.append(board[i][j])
+    for t in temp:
+        answer += t
     return answer
