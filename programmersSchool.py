80d6de3de119d2e9c0f5d9107a4580639625f17f
frogEUN <eunhasu801@gmail.com>
2023-08-25 23:32:23 +0900

    코딩테스트 입문 Day1

--- programmersSchool.py
+++ programmersSchool.py
@@ -1,40 +1,22 @@
-# 코딩 기초 트레이닝
+# 코딩 테스트 입문
 
-# 특별한 이차원 배열 2
-def solution(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)):
-            if arr[i][j] != arr[j][i]:
-                return 0
-    return 1
+# 두 수의 합
+def solution(num1, num2):
+    return num1 + num2
 
 
-# 정사각형으로 만들기
-def solution(arr):
-    a = len(arr)
-    b = len(arr[0])
-    if a > b:
-        for i in range(len(arr)):
-            for k in range(a - b):
-                arr[i].append(0)
-    elif a < b:
-        for i in range(b - a):
-            temp = []
-            for k in range(b):
-                temp.append(0)
-            arr.append(temp)
-            temp = []
-    return arr
+solution = lambda *x: sum(x)
+solution=lambda x, y: x + y
 
 
-# 이차원 배열 대각선 순회하기
-def solution(board, k):
-    answer = 0
-    temp = []
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if i + j <= k:
-                temp.append(board[i][j])
-    for t in temp:
-        answer += t
-    return answer
+# 두 수의 차
+solution = lambda num1, num2: num1 - num2
+
+
+# 두 수의 곱
+solution = lambda num1, num2: num1 * num2
+
+
+# 몫 구하기
+def solution(num1, num2):
+    return num1 // num2
